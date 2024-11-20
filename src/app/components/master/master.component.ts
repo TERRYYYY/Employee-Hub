5b4e96c12a8b61741b270a7f2994ce5e9c9c8a34
TERRYYYY <kariuki.terry11@gmail.com>
2024-11-20 12:37:06 +0300

    Using *ngIf directive

--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RolesComponent } from '../roles/roles.component';
+import { DesignationComponent } from '../designation/designation.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [],
+  imports: [RolesComponent, DesignationComponent, CommonModule],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })
 export class MasterComponent {
+
+  // Stores the name of current component clicked
+  currentComponent: string = ""
+
+  //Function to the change between the 2 tabs
+  changeTab(tabName: string){
+    this.currentComponent = tabName;
+  }
 
 }
