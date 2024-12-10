a9d13be9dad5defdf233d63c12ee0365a37528f4
TERRYYYY <kariuki.terry11@gmail.com>
2024-12-10 14:57:44 +0300

    Added OnInit method & added API calling code

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,22 +9,38 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  firstName: string = "Angular Tutorial";
+export class RolesComponent implements OnInit{
 
-  version: number = 17;
+  //Variable where the data in our API link will be stored. 
+  roleList: any [] = [];
 
-  isActive: boolean = false;
+  //Inject our HTTPClient
+  http = inject(HttpClient);
 
-  currentDate: Date = new Date();
-
-  selectedCounty: string = " ";
-
-  showWelcomeAlert(){
-    alert ("Welcome to Angular Tutorial");
+  ngOnInit():void{
   }
 
-  showMessage(message: string){
-    alert(message);
+  //Gets All Roles
+  getAllRoles(){
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles") .subscribe((res: any) =>{
+      console.log(res);
+    })
   }
+  // firstName: string = "Angular Tutorial";
+
+  // version: number = 17;
+
+  // isActive: boolean = false;
+
+  // currentDate: Date = new Date();
+
+  // selectedCounty: string = " ";
+
+  // showWelcomeAlert(){
+  //   alert ("Welcome to Angular Tutorial");
+  // }
+
+  // showMessage(message: string){
+  //   alert(message);
+  // }
 }
